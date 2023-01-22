b99eff56d8be83e29b62e0273fbe744caa99baff
vsvirydovich <v.sviridovich@imedia.by>
2023-01-22 18:34:22 +0300

    optimization

--- app/scripts/components/showModal.js
+++ app/scripts/components/showModal.js
@@ -3,7 +3,18 @@ import { Fancybox } from '@fancyapps/ui';
 const showModal = () => {
   document.querySelectorAll('.statistics__btn').forEach((btn) => {
     btn.addEventListener('click', () => {
+      const link = document.head.querySelector('link[data-link="fancybox"]');
+
+      link?.remove();
+
       Fancybox.show([{ src: '#modal_login', type: 'inline' }]);
+
+      const script = document.createElement('link');
+      script.setAttribute('rel', 'stylesheet');
+      script.setAttribute('href', './assets/fancybox.min.css');
+      script.dataset.link = 'fancybox';
+
+      document.head.prepend(script);
     });
   });
 };
--- app/scripts/components/sidebarSlider.js
+++ app/scripts/components/sidebarSlider.js
@@ -3,7 +3,9 @@ import Swiper, { Autoplay } from 'swiper';
 const sidebarSlider = () => {
   new Swiper('.sidebar-slider', {
     modules: [Autoplay],
+    spaceBetween: 20,
     direction: 'vertical',
+    loop: true,
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
