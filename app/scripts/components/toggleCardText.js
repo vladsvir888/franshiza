398e2a1a9d5d0caf81d7c8ecc618c75eeeac93f7
vsvirydovich <v.sviridovich@imedia.by>
2023-01-24 00:07:06 +0300

    small fix

--- app/scripts/components/toggleCardText.js
+++ app/scripts/components/toggleCardText.js
@@ -1,7 +1,16 @@
 const toggleCardText = () => {
   document.querySelectorAll('.card-block__toggle').forEach((btn) => {
     btn?.addEventListener('click', () => {
-      btn.closest('.card-block').classList.toggle('card-block--show');
+      const closestEl = btn.closest('.card-block');
+      const text = btn.querySelector('.btn__text');
+
+      if (!closestEl.classList.contains('card-block--show')) {
+        closestEl.classList.add('card-block--show');
+        text.textContent = 'Свернуть';
+      } else {
+        closestEl.classList.remove('card-block--show');
+        text.textContent = 'Развернуть';
+      }
     });
   });
 };
