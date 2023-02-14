da7615b4ff316d3db9f200770151b52036bc4942
vsvirydovich <v.sviridovich@imedia.by>
2023-02-14 15:29:11 +0300

    mask for input-date

--- app/scripts/components/datepicker.js
+++ app/scripts/components/datepicker.js
@@ -1,12 +1,32 @@
 import AirDatepicker from 'air-datepicker';
+import IMask from 'imask';
 
 const initDatepicker = () => {
   document.querySelectorAll('.input-datepicker').forEach((input) => {
-    new AirDatepicker(input, {
+    const calendar = new AirDatepicker(input, {
       selectedDates: input.value,
       onSelect({ datepicker }) {
         datepicker.hide();
       },
+    });
+
+    IMask(
+      input, {
+        mask: Date,
+        lazy: false,
+      },
+    );
+
+    input.addEventListener('change', () => {
+      const { value } = input;
+
+      const arr = value.split('.');
+
+      const newArr = [arr[2], arr[1], arr[0]];
+
+      const str = newArr.join('-');
+
+      calendar.selectDate(str);
     });
   });
 };
