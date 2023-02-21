46a505db5ba62f4d631d0f8ffee8506e74b8abae
vsvirydovich <v.sviridovich@imedia.by>
2023-02-21 11:05:05 +0300

    fix

--- app/scripts/components/sidebarSlider.js
+++ app/scripts/components/sidebarSlider.js
@@ -6,6 +6,7 @@ const sidebarSlider = () => {
     spaceBetween: 20,
     direction: 'vertical',
     loop: true,
+    speed: 900,
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
